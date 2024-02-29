2211d7c100acd98d58693eb696de20ed7c902bb9
ATartarus <kolyapryazko@gmail.com>
2024-02-29 17:06:44 +0300

    init

---
+++ BitmapEditor/EditorControl.xaml.cs
@@ -0,0 +1,271 @@
+﻿using System.Drawing.Imaging;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using win = System.Windows;
+
+
+namespace BitmapEditor
+{
+    /// <summary>
+    /// Interaction logic for EditorControl.xaml
+    /// </summary>
+    public partial class EditorControl : UserControl, INotifyPropertyChanged
+    {
+        private WriteableBitmap bitmap;
+        private byte[] rawBitmapData;
+        private win.Size bitmapPixelSize;
+
+        private (int X, int Y) selectedPixel;
+        private win.Shapes.Rectangle selectionRect;
+        private double scrollFactor = 0.2;
+        private win.Point lastMousePos;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public WriteableBitmap Bitmap { get => bitmap; }
+        public byte[] RawBitmapData { get => (byte[])rawBitmapData.Clone(); }
+        private win.Size BitmapPixelSize
+        {
+            get => bitmapPixelSize;
+            set
+            {
+                bitmapPixelSize = value;
+                selectionRect.Width = bitmapPixelSize.Width;
+                selectionRect.Height = bitmapPixelSize.Height;
+            }
+        }
+        public int BytesPerPixel { get; private set; }
+        public int Stride { get; private set; }
+
+        public (int X, int Y) SelectedPixel
+        {
+            get => selectedPixel;
+            set
+            {
+                selectedPixel = value;
+                if (!selectionRect.IsVisible) selectionRect.Visibility = Visibility.Visible;
+                UpdateSelectionRectPosition();
+                OnPropertyChanged();
+                OnPropertyChanged("SelectedPixelValue");
+            }
+        }
+        public win.Media.Color SelectedPixelValue
+        {
+            get
+            {
+                int ind = SelectedPixel.Y * Stride + SelectedPixel.X * BytesPerPixel;
+                win.Media.Color color = new()
+                {
+                    R = rawBitmapData[ind + 2],
+                    G = rawBitmapData[ind + 1],
+                    B = rawBitmapData[ind],
+                };
+                return color;
+            }
+        }
+
+
+        public EditorControl()
+        {
+            InitializeComponent();
+            Load(@"C:\BNTU\Image Processing\IPLab1\IPLab1\black.bmp");
+
+            selectionRect = new()
+            {
+                StrokeThickness = 1,
+                Stroke = win.Media.Brushes.Black
+            };
+            BitmapCanvas.Children.Add(selectionRect);
+            selectionRect.Visibility = Visibility.Hidden;
+        }
+
+        public void Load(string uri)
+        {
+            BitmapImage bmp = new();
+            bmp.BeginInit();
+            bmp.UriSource = new Uri(uri, UriKind.RelativeOrAbsolute);
+            bmp.EndInit();
+
+            bitmap = new(bmp);
+
+            Stride = Bitmap.PixelWidth * ((Bitmap.Format.BitsPerPixel + 7) / 8);
+            BytesPerPixel = (Bitmap.Format.BitsPerPixel + 7) / 8;
+
+
+            rawBitmapData = new byte[Bitmap.PixelHeight * Stride];
+            Bitmap.CopyPixels(rawBitmapData, Stride, 0);
+
+            BitmapPreview.Source = Bitmap;
+        }
+
+        public void Save(string uri)
+        {
+            Bitmap bmp = new Bitmap(Bitmap.PixelWidth, Bitmap.PixelHeight, PixelFormat.Format32bppArgb);
+
+            BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, Bitmap.PixelWidth, Bitmap.PixelHeight),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            Bitmap.WritePixels(new Int32Rect(0, 0, Bitmap.PixelWidth, Bitmap.PixelHeight), rawBitmapData, Stride, 0);
+            Bitmap.CopyPixels(Int32Rect.Empty, bitmapData.Scan0, bitmapData.Height * bitmapData.Stride, bitmapData.Stride);
+
+            bmp.UnlockBits(bitmapData);
+
+            bmp.Save(uri, ImageFormat.Bmp);
+            bmp.Dispose();
+        }
+
+        public unsafe void ChangeMask(int colorOffset, bool hide)
+        {
+            //Bitmap.Lock();
+
+            //byte* bitmapBuffer = (byte*)Bitmap.BackBuffer.ToPointer();
+            //int bytesPerPixel = (Bitmap.Format.BitsPerPixel + 7) / 8;
+
+            //for (int y = 0; y < Bitmap.PixelHeight; y++)
+            //{
+            //    for (int x = 0; x < Bitmap.PixelWidth; x++)
+            //    {
+            //        int offset = y * Stride + x * bytesPerPixel;
+
+            //        bitmapBuffer[offset + colorOffset] = hide ? (byte)0 : rawBitmapData[offset + colorOffset];
+            //    }
+            //}
+
+            //Bitmap.AddDirtyRect(new Int32Rect(0, 0, Bitmap.PixelWidth, Bitmap.PixelHeight));
+            //Bitmap.Unlock();
+        }
+
+        public unsafe void ChangePixel(int colorOffset, byte colorValue)
+        {
+            Bitmap.Lock();
+
+            byte* bitmapBuffer = (byte*)Bitmap.BackBuffer.ToPointer();
+            int bytesPerPixel = (Bitmap.Format.BitsPerPixel + 7) / 8;
+
+            int index = SelectedPixel.Y * Stride + SelectedPixel.X * bytesPerPixel;
+            bitmapBuffer[index + colorOffset] = colorValue;
+            rawBitmapData[index + colorOffset] = colorValue;
+
+            Bitmap.AddDirtyRect(new Int32Rect(SelectedPixel.X, SelectedPixel.Y, 1, 1));
+            Bitmap.Unlock();
+
+            OnPropertyChanged("SelectedPixelValue");
+        }
+
+        private void OnPixelClick(object sender, MouseButtonEventArgs e)
+        {
+            win.Point pos = e.GetPosition(BitmapPreview);
+            SelectedPixel = new(
+                (int)(pos.X / BitmapPixelSize.Width),
+                (int)(pos.Y / BitmapPixelSize.Height)
+            );
+        }
+
+        private void Canvas_OnScroll(object sender, MouseWheelEventArgs e)
+        {
+            (double X, double Y) offset = new(
+                e.Delta * scrollFactor,
+                e.Delta * scrollFactor
+            );
+
+            BitmapPreview.Width = BitmapPreview.ActualWidth + offset.X;
+            BitmapPreview.Height = BitmapPreview.ActualHeight + offset.Y;
+
+            BitmapPixelSize = new win.Size(
+                BitmapPreview.Width / Bitmap.PixelWidth,
+                BitmapPreview.Height / Bitmap.PixelHeight
+            );
+            if (BitmapPreview.Width < BitmapPreview.MinWidth)
+            {
+                BitmapPixelSize = new win.Size(
+                    BitmapPreview.MinWidth / Bitmap.PixelWidth,
+                    BitmapPreview.MinHeight / Bitmap.PixelHeight
+                );
+            }
+
+            if ((int)BitmapPreview.ActualHeight == (int)BitmapPreview.MinHeight && offset.Y < 0) return;
+
+            win.Point mousePos = e.GetPosition(this);
+            (double X, double Y) shiftRatio = new(
+                (mousePos.X - Canvas.GetLeft(BitmapPreview)) / BitmapPreview.ActualWidth,
+                (mousePos.Y - Canvas.GetTop(BitmapPreview)) / BitmapPreview.ActualHeight
+            );
+
+            if ((shiftRatio.X < 0 || shiftRatio.X > 1) ||
+                (shiftRatio.Y < 0 || shiftRatio.Y > 1))
+            {
+                shiftRatio.X = 0.5;
+                shiftRatio.Y = 0.5;
+            }
+
+            MoveElement(BitmapPreview, (-offset.X * shiftRatio.X, -offset.Y * shiftRatio.Y));
+            UpdateSelectionRectPosition();
+        }
+
+        private void Canvas_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Canvas.SetLeft(BitmapPreview, e.NewSize.Width / 2 - BitmapPreview.ActualWidth / 2);
+            Canvas.SetTop(BitmapPreview, e.NewSize.Height / 2 - BitmapPreview.ActualHeight / 2);
+        }
+
+        private void Canvas_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.RightButton != MouseButtonState.Pressed) return;
+
+            win.Point currentPos = e.GetPosition(this);
+            (double X, double Y) delta = new(
+                currentPos.X - lastMousePos.X,
+                currentPos.Y - lastMousePos.Y
+            );
+
+            MoveElement(BitmapPreview, delta);
+            UpdateSelectionRectPosition();
+            lastMousePos = currentPos;
+        }
+
+        private void Canvas_OnRightMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            lastMousePos = e.GetPosition(this);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            selectionRect.Height = BitmapPreview.ActualHeight / Bitmap.PixelHeight;
+            selectionRect.Width = BitmapPreview.ActualWidth / Bitmap.PixelWidth;
+
+            BitmapPixelSize = new win.Size(
+                BitmapPreview.ActualWidth / Bitmap.PixelWidth,
+                BitmapPreview.ActualHeight / Bitmap.PixelHeight
+            );
+        }
+
+        private void UpdateSelectionRectPosition()
+        {
+            if (!this.IsLoaded) return;
+
+            win.Point bitmapPos = new(
+                Canvas.GetLeft(BitmapPreview),
+                Canvas.GetTop(BitmapPreview)
+            );
+
+            Canvas.SetLeft(selectionRect, bitmapPos.X + SelectedPixel.X * BitmapPixelSize.Width);
+            Canvas.SetTop(selectionRect, bitmapPos.Y + SelectedPixel.Y * bitmapPixelSize.Height);
+        }
+
+        private static void MoveElement(UIElement element, (double x, double y) distance)
+        {
+            Canvas.SetLeft(element, Canvas.GetLeft(element) + distance.x);
+            Canvas.SetTop(element, Canvas.GetTop(element) + distance.y);
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ BitmapEditor/MainWindow.xaml.cs
@@ -0,0 +1,325 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BitmapEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            BitmapEditor.PropertyChanged += OnSelectedPixelChanged;
+
+            BindBitmapToDataGrids();
+        }
+
+        private void OnTextInput(object sender, KeyEventArgs e)
+        {
+            if (sender is TextBox textBox)
+            {
+                string str = textBox.Text;
+                if (e.Key >= Key.D0 && e.Key <= Key.D9) str += (e.Key - Key.D0);
+                else if (e.Key == Key.Back && str.Length > 0) str = str.Substring(0, str.Length - 1);
+                if (str == "") str = "0";
+                if (byte.TryParse(str, out byte color))
+                {
+                    int colorOffset = 0;
+                    if (sender == GreenValue) colorOffset = 1;
+                    else if (sender == RedValue) colorOffset = 2;
+
+                    BitmapEditor.ChangePixel(colorOffset, color);
+                }
+            }
+        }
+
+        private void OnBitmapMaskChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is not CheckBox) return;
+
+            int colorOffset = 0;
+            if (sender == GreenMask) colorOffset = 1;
+            else if (sender == RedMask) colorOffset = 2;
+
+            bool hide = !((CheckBox)sender).IsChecked ?? false;
+
+            BitmapEditor.ChangeMask(colorOffset, hide);
+        }
+
+        private void OnOpenFileClick(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new Microsoft.Win32.OpenFileDialog();
+            filePicker.FileName = "Bitmap";
+            filePicker.DefaultExt = ".bmp";
+            filePicker.Filter = "Bitmap files (.bmp)|*.bmp";
+
+            bool? result = filePicker.ShowDialog();
+
+            if (result == true)
+            {
+                BitmapEditor.Load(filePicker.FileName);
+            }
+        }
+
+        private void OnSaveFileClick(object sender, RoutedEventArgs e)
+        {
+            var filePicker = new Microsoft.Win32.SaveFileDialog();
+            filePicker.FileName = "Bitmap";
+            filePicker.DefaultExt = ".bmp";
+            filePicker.Filter = "Bitmap files (.bmp)|*.bmp";
+
+            bool? result = filePicker.ShowDialog();
+
+            if (result == true)
+            {
+                BitmapEditor.Save(filePicker.FileName);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            double maxWidth = 0;
+            foreach (TabItem tabItem in SideBar.Items)
+            {
+                FrameworkElement? content = tabItem.Content as FrameworkElement;
+                content?.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                maxWidth = Math.Max(maxWidth, content?.DesiredSize.Width ?? 0);
+            }
+
+            SideBar.Width = maxWidth;
+
+            foreach (TabItem tabitem in SideBar.Items)
+            {
+                tabitem.Width = maxWidth / SideBar.Items.Count;
+            }
+        }
+
+        private void BindBitmapToDataGrids()
+        {
+            byte[] bitmapData = BitmapEditor.RawBitmapData;
+
+            bool black = true;
+            for (int i = 0; i < bitmapData.Length; i++)
+            {
+                if (bitmapData[i] != 0 && bitmapData[i] != 255) black = false;
+            }
+
+            if (black)
+            {
+                ColorMatrices.IsEnabled = false;
+                ColorMatrices.Visibility = Visibility.Hidden;
+                BlackMatrices.IsEnabled = true;
+                BlackMatrices.Visibility = Visibility.Visible;
+
+                DataTable dataTable = new DataTable();
+
+                for (int i = 0; i < BitmapEditor.Bitmap.PixelWidth; i++)
+                {
+                    dataTable.Columns.Add(null, typeof(byte));
+                }
+
+                for (int i = 0; i < BitmapEditor.Bitmap.PixelHeight; i++)
+                {
+                    DataRow row = dataTable.NewRow();
+                    for (int j = 0; j < BitmapEditor.Bitmap.PixelWidth; j++)
+                    {
+                        row[j] = bitmapData[i * BitmapEditor.Stride + j * BitmapEditor.BytesPerPixel] > 0 ? 0 : 1;
+                    }
+                    dataTable.Rows.Add(row);
+                }
+
+                dataTable.ColumnChanged += (sender, e) =>
+                {
+                    if (e.ProposedValue is byte color)
+                    {
+                        BitmapEditor.ChangePixel(0, color == (byte)0 ? (byte)255 : (byte)0);
+                        BitmapEditor.ChangePixel(1, color == (byte)0 ? (byte)255 : (byte)0);
+                        BitmapEditor.ChangePixel(2, color == (byte)0 ? (byte)255 : (byte)0);
+                    }
+                };
+
+                BlackMatrix.ItemsSource = dataTable.DefaultView;
+            }
+            else
+            {
+                for (int n = 0; n < 3; n++)
+                {
+                    DataTable dataTable = new DataTable();
+
+                    for (int i = 0; i < BitmapEditor.Bitmap.PixelWidth; i++)
+                    {
+                        dataTable.Columns.Add(null, typeof(byte));
+                    }
+
+                    for (int i = 0; i < BitmapEditor.Bitmap.PixelHeight; i++)
+                    {
+                        DataRow row = dataTable.NewRow();
+                        for (int j = 0; j < BitmapEditor.Bitmap.PixelWidth; j++)
+                        {
+                            row[j] = bitmapData[i * BitmapEditor.Stride + j * BitmapEditor.BytesPerPixel + n];
+                        }
+                        dataTable.Rows.Add(row);
+                    }
+
+                    int offset = n;
+                    dataTable.ColumnChanged += (sender, e) =>
+                    {
+                        if (e.ProposedValue is byte color)
+                        {
+                            BitmapEditor.ChangePixel(offset, color);
+                        }
+                    };
+
+                    switch (n)
+                    {
+                        case 0:
+                            BlueMatrix.ItemsSource = dataTable.DefaultView;
+                            break;
+                        case 1:
+                            GreenMatrix.ItemsSource = dataTable.DefaultView;
+                            break;
+                        case 2:
+                            RedMatrix.ItemsSource = dataTable.DefaultView;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void DataGrid_OnScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (e.OriginalSource is ScrollViewer scroll)
+            {
+                if (new DataGrid[] { RedMatrix, GreenMatrix, BlueMatrix }
+                    .FirstOrDefault(grid => FindScrollViewer(grid)?.Equals(scroll) ?? false) != null)
+                {
+                    ScrollGrids(scroll.VerticalOffset, scroll.HorizontalOffset);
+                }
+            }
+        }
+
+        private static ScrollViewer? FindScrollViewer(DependencyObject obj)
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child is ScrollViewer viewer) return viewer;
+                else
+                {
+                    ScrollViewer? childOfChild = FindScrollViewer(child);
+                    if (childOfChild != null)
+                    {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void DataGrid_OnSelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            DataGrid senderGrid = (DataGrid)sender;
+            DataGridCellInfo selectedCell = senderGrid.SelectedCells[0];
+
+            int row = senderGrid.Items.IndexOf(selectedCell.Item);
+            int col = selectedCell.Column.DisplayIndex;
+
+            SelectGridsCell(row, col);
+            BitmapEditor.PropertyChanged -= OnSelectedPixelChanged;
+            BitmapEditor.SelectedPixel = (col, row);
+            BitmapEditor.PropertyChanged += OnSelectedPixelChanged;
+        }
+
+        private void OnSelectedPixelChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SelectedPixel")
+            {
+                SelectGridsCell(BitmapEditor.SelectedPixel.Y, BitmapEditor.SelectedPixel.X);
+                ScrollGridsIntoView();
+            }
+        }
+
+        private void SelectGridsCell(int row, int col)
+        {
+            DataGrid[] grids;
+            if (BlackMatrix.IsEnabled) grids = [BlackMatrix];
+            else grids = [RedMatrix, GreenMatrix, BlueMatrix];
+            foreach (DataGrid grid in grids)
+            {
+                grid.SelectedCellsChanged -= DataGrid_OnSelectedCellsChanged;
+            }
+
+            foreach (var grid in grids)
+            {
+                grid.SelectedCells.Clear();
+                grid.SelectedCells.Add(new DataGridCellInfo(grid.Items[row], grid.Columns[col]));
+            }
+
+            foreach (DataGrid grid in grids)
+            {
+                grid.SelectedCellsChanged += DataGrid_OnSelectedCellsChanged;
+            }
+        }
+
+        private void ScrollGrids(double? verticalOffset, double? horizontalOffset)
+        {
+            DataGrid[] grids;
+            if (BlackMatrix.IsEnabled) grids = [BlackMatrix];
+            else grids = [RedMatrix, GreenMatrix, BlueMatrix];
+            ScrollViewer?[] scrolls = new ScrollViewer[grids.Length];
+
+            for (int i = 0; i < scrolls.Length; i++)
+            {
+                scrolls[i] = FindScrollViewer(grids[i]);
+                if (scrolls[i] != null) scrolls[i].ScrollChanged -= DataGrid_OnScrollChanged;
+            }
+
+            foreach (var scroll in scrolls)
+            {
+                if (verticalOffset != null) scroll?.ScrollToVerticalOffset(verticalOffset.Value);
+                if (horizontalOffset != null) scroll?.ScrollToHorizontalOffset(horizontalOffset.Value);
+            }
+
+            foreach (var scroll in scrolls)
+            {
+                if (scroll != null) scroll.ScrollChanged += DataGrid_OnScrollChanged;
+            }
+        }
+
+        private void ScrollGridsIntoView()
+        {
+            Size offset = new(
+                BitmapEditor.SelectedPixel.X * RedMatrix.ColumnWidth.Value,
+                BitmapEditor.SelectedPixel.Y * RedMatrix.RowHeight
+            );
+            Size half = new(
+                RedMatrix.ActualWidth / 2 - RedMatrix.ColumnWidth.Value / 2,
+                RedMatrix.ActualHeight / 2 - RedMatrix.RowHeight / 2
+            );
+
+            if (offset.Width - half.Width > 0) offset.Width -= half.Width;
+            else offset.Width = 0;
+            if (offset.Height - half.Height > 0) offset.Height -= half.Height;
+            else offset.Height = 0;
+
+            ScrollGrids(offset.Height, offset.Width);
+        }
+
+        private void SideBar_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems[0] == MatrixEditorTab)
+                BitmapEditor.PropertyChanged += OnSelectedPixelChanged;
+            else
+                BitmapEditor.PropertyChanged -= OnSelectedPixelChanged;
+        }
+    }
+}
